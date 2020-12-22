73c8e54f4e5e67c2e53f1817bc731502e44932fb
anikatahsin12 <bushratahsin653@gmail.com>
2020-12-23 03:21:27 +0600

    another

--- store/static/js/search.js
+++ store/static/js/search.js
@@ -2,9 +2,9 @@ var searchbar = document.querySelector(".search");
 
 function slide(){
     if(searchbar.classList.contains("hide")){
-        searchbar.classList.remove("hide")
+        searchbar.classList.remove("hide");
     }
     else{
-        searchbar.classList.add("hide")
+        searchbar.classList.add("hide");
     }
 }
