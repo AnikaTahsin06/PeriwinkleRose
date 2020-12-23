96d5c538e286f1ee0cc13a29a5ea17369f149044
anikatahsin12 <bushratahsin653@gmail.com>
2020-12-23 18:19:23 +0600

    changeed django project name

--- Periwinkle_Rose/asgi.py
+++ Periwinkle_Rose/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Ladies_Hut project.
+ASGI config for Periwinkle_Rose project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ladies_Hut.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Periwinkle_Rose.settings')
 
 application = get_asgi_application()
--- Periwinkle_Rose/wsgi.py
+++ Periwinkle_Rose/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Ladies_Hut project.
+WSGI config for Periwinkle_Rose project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ladies_Hut.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Periwinkle_Rose.settings')
 
 application = get_wsgi_application()
